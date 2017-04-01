b726613e422ea61dc2a1f3f9f8138e8980b55ee7
zhaofei0129 <494709671@qq.com>
2017-04-01 21:42:40 +0800

    finish the 1st picture

--- Practice/HomePageViewController.m
+++ Practice/HomePageViewController.m
@@ -13,6 +13,9 @@
 
 @property (weak, nonatomic) IBOutlet UIButton *loginBtn;
 @property (weak, nonatomic) IBOutlet UIButton *registerBtn;
+@property (weak, nonatomic) IBOutlet UIButton *btn0;
+@property (weak, nonatomic) IBOutlet UIButton *btn1;
+@property (weak, nonatomic) IBOutlet UIButton *btn2;
 
 @end
 
@@ -34,20 +37,76 @@
     self.registerBtn.backgroundColor = UIColorFromHex(0x00be00);
     self.registerBtn.layer.masksToBounds = YES;
     self.registerBtn.layer.cornerRadius = 2.0;
-}
+    
+    // draw btn0, btn1, btn2
+    [self.btn0 setBackgroundImage:[self imageWithColor:UIColorFromHex(0xaaaaaa)] forState:UIControlStateNormal];
+    [self.btn0 setBackgroundImage:[self imageWithColor:UIColorFromHex(0xe5e5e5)] forState:UIControlStateSelected];
+    self.btn0.layer.masksToBounds = YES;
+    self.btn0.layer.cornerRadius = 4.0;
+    self.btn0.selected = YES;
 
+    [self.btn1 setBackgroundImage:[self imageWithColor:UIColorFromHex(0xaaaaaa)] forState:UIControlStateNormal];
+    [self.btn1 setBackgroundImage:[self imageWithColor:UIColorFromHex(0xe5e5e5)] forState:UIControlStateSelected];
+    self.btn1.layer.masksToBounds = YES;
+    self.btn1.layer.cornerRadius = 4.0;
+    
+    [self.btn2 setBackgroundImage:[self imageWithColor:UIColorFromHex(0xaaaaaa)] forState:UIControlStateNormal];
+    [self.btn2 setBackgroundImage:[self imageWithColor:UIColorFromHex(0xe5e5e5)] forState:UIControlStateSelected];
+    self.btn2.layer.masksToBounds = YES;
+    self.btn2.layer.cornerRadius = 4.0;
+
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (UIImage *)imageWithColor:(UIColor *)color {
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
 - (IBAction)loginBtnPressed:(UIButton *)sender {
     NSLog(@"loginBtnPressed");
+    sender.layer.borderColor = UIColorFromHex(0x00be00).CGColor;
 }
+
+- (IBAction)loginBtnPressedHighlighted:(UIButton *)sender {
+    NSLog(@"loginBtnPressedHighlighted");
+    sender.layer.borderColor = UIColorFromHex(0x00ab00).CGColor;
+}
+
 
 - (IBAction)registerBtnPressed:(UIButton *)sender {
     NSLog(@"registerBtnPressed");
+    sender.backgroundColor = UIColorFromHex(0x00be00);
 }
 
+- (IBAction)registerBtnPressedHighlighted:(UIButton *)sender {
+    NSLog(@"registerBtnPressedHighlighted");
+    sender.backgroundColor = UIColorFromHex(0x00ab00);
+}
+
+- (IBAction)btn0Pressed:(UIButton *)sender {
+    if (sender.tag == 10) {
+        self.btn0.selected = YES;
+        self.btn1.selected = NO;
+        self.btn2.selected = NO;
+    } else if (sender.tag == 11) {
+        self.btn0.selected = NO;
+        self.btn1.selected = YES;
+        self.btn2.selected = NO;
+    } else {
+        self.btn0.selected = NO;
+        self.btn1.selected = NO;
+        self.btn2.selected = YES;
+    }
+}
 @end
